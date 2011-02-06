79679a4cf618b7d3d268ff1d0a7642b89c0c09ca
Jonathan Rockway <jon@jrock.us>
2011-02-06 02:56:55 -0600

    add benchmark thingie

---
+++ bench.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+use Hiredis::Async::AnyEvent;
+use feature 'say';
+
+my $key = 'OHHAI';
+my $value = 'lolcat';
+my $i = 300_000;
+my $ii = $i;
+my $done = AE::cv;
+my $redis = Hiredis::Async::AnyEvent->new;
+
+my $set; $set = sub {
+    $i--;
+    $redis->Command(['SET', $key.$i, $value], $i < 0 ? $done : $set);
+};
+$set->() for 1..100;
+
+my $timer = AnyEvent->timer( after => 3, interval => 3, cb => sub {
+    say "$i items remaining";
+});
+
+my $start = AnyEvent->now;
+$done->recv;
+my $end = AnyEvent->now;
+
+say "\aIt took ". ($end - $start). " seconds";
+say " that is ". ($ii/($end - $start)). " per second"
+
